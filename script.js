b3189100839c9e996317fd1b7236876728378f68
Kiuvo <89743893+Kiuvos@users.noreply.github.com>
2024-04-07 19:44:24 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+customElements.define('etiqueta-html',
+  class extends HTMLElement {
+      constructor() {
+          super();
+      }
+      connectedCallback() {
+          fetch(this.getAttribute("src"))
+              .then(r => r.text())
+              .then(t => {
+                  let parser = new DOMParser();
+                  let html = parser.parseFromString(t, "text/html");
+                  this.innerHTML = html.body.innerHTML;
+              }).catch(e => console.error(e));
 
+      }
+});
